f1d04bec7a11c91b28b378304a611a4972f2fe15
Jake <jschoff155@gmail.com>
2023-05-12 21:37:06 -0500

    added packages and components

--- src/App.js
+++ src/App.js
@@ -1,49 +1,17 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import HomepageCarousel from './HomepageCarousel';
-import PriorityTopics from './PriorityTopics';
-import BeforeTheBaby from './BeforeTheBaby';
+import HeaderandNav from "./HeaderandNav";
+import HomepageCarousel from "./HomepageCarousel";
+import PriorityTopics from "./PriorityTopics";
+import BeforeTheBaby from "./BeforeTheBaby";
 
-
-
-const Header = () => {
-	return (
-		<>
-    <h2>The Parenting Protocol</h2>
-    <ul>
-      <li><a href=''>About</a></li>
-      <li><a href='#'>User Settings</a></li>
-      <button>Dark/Light Mode</button>
-    </ul>
-    <br></br>
-    <ul>
-      <li><a href='#'>Must Haves</a></li>
-      <li><a href='#'>Meal Plans</a></li>
-      <li><a href='#'>Staying Active</a></li>
-      <li><a href='#'>Deals</a></li>
-    </ul>
+function App() {
+  return (
+    <>
+    <HeaderandNav />
+    <HomepageCarousel />
+    <PriorityTopics />
+    <BeforeTheBaby/>
     </>
-	)
-};
-
-const Homepage = () => {
-	return (
-		<div>
-				<Header/>
-        <HomepageCarousel />
-        <PriorityTopics />
-        <BeforeTheBaby />
-		</div>
-	)
-};
-
-const Aboutpage = () => {
-	return (
-		<div>
-				<Header/>
-				<h1>Aboutpage</h1>
-				<Link to='/'>Go to Aboutpage</Link>
-		</div>
-	)
-};
-export {Homepage} ;
+    
+  );
+}
+export default App;
--- src/HeaderandNav.js
+++ src/HeaderandNav.js
@@ -2,22 +2,19 @@ import React from 'react'
 import './HeaderandNav.css'
 
 export default function HeaderandNav() {
-  
-  
     return (
     <>
     <h2>The Parenting Protocol</h2>
     <ul>
-      <li><a href=''>About</a></li>
-      <li><a href='#'>User Settings</a></li>
+      <li><button>About</button></li>
       <button>Dark/Light Mode</button>
     </ul>
     <br></br>
     <ul>
-      <li><a href='#'>Must Haves</a></li>
-      <li><a href='#'>Meal Plans</a></li>
-      <li><a href='#'>Staying Active</a></li>
-      <li><a href='#'>Deals</a></li>
+      <li>Must Haves</li>
+      <li>Meal Plans</li>
+      <li>Staying Active</li>
+      <li>Deals</li>
     </ul>
     </>
   )
